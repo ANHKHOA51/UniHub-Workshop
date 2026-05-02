1eb01a600fb5f80ab92dc30a94d92ba2277affc4
hlam3004 <hlam.30042005@gmail.com>
2026-05-02 16:43:35 +0700

    add test jwt

--- server/src/routes/health.route.js
+++ server/src/routes/health.route.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import * as jwtHelper from '../utils/jwt.helper.js';
+import * as authMiddleware from '../middlewares/auth.middleware.js';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -6,5 +9,24 @@ router.get('/', (req, res) => {
         message: 'OK'
     })
 });
+
+router.post('/auth', (req, res) => {
+    const payload = {
+        id: '1',
+        name: 'test'
+    };
+
+    const token = jwtHelper.generateToken(payload, process.env.JWT_SECRET, "1h");
+
+    return res.json({
+        token: token
+    });
+})
+
+router.get('/auth', authMiddleware.isAuthenticated, (req, res) => {
+    res.json({
+        susscess: true
+    });
+})
 
 export default router;
