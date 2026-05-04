4df1853dd7056b2b451eeaf8c8453850449695f3
ntritran999 <ntritran999@gmail.com>
2026-05-04 21:47:55 +0700

    Add create, update, delete workshop apis for admin

--- server/src/controllers/workshop.controller.js
+++ server/src/controllers/workshop.controller.js
@@ -22,3 +22,52 @@ export const getWorkshop = async (req, res) => {
         });
     }
 };
+
+export const updateWorkshop = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const updatedWorkshop = await workshopService.updateWorkshop(id, req.body);
+        res.status(200).json({
+            message: 'Workshop updated successfully',
+            workshop: updatedWorkshop
+        });
+    } catch (error) {
+        res.status(error.status || 500).json({
+            message: error.message || 'Failed to update workshop'
+        });
+    }
+};
+
+export const createWorkshop = async (req, res) => {
+    try {
+        const workshopData = req.body;
+        const pdfFile = req.files?.pdf?.[0];
+        const floorPlanFile = req.files?.floor_plan?.[0];
+
+        const newWorkshop = await workshopService.createWorkshop(workshopData, pdfFile, floorPlanFile);
+
+        res.status(201).json({
+            message: 'Workshop created successfully',
+            workshop: newWorkshop
+        });
+    } catch (error) {
+        res.status(error.status || 500).json({
+            message: error.message || 'Failed to create workshop'
+        });
+    }
+};
+
+export const deleteWorkshop = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await workshopService.deleteWorkshop(id);
+        res.status(200).json({
+            message: 'Workshop deleted successfully'
+        });
+    } catch (error) {
+        res.status(error.status || 500).json({
+            message: error.message || 'Failed to delete workshop'
+        });
+    }
+};
